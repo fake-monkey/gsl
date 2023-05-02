69e43f595557a54fff1b4c1fa13c5be395610014
FakeMonkey <msolwooden@outlook.com>
2023-05-02 17:46:40 +0800

    屈服了，直接使用cmake将依赖项进行安装，但pdb文件不会复制，调试可能会出现问题。

--- generate_debug_x64.bat
+++ generate_debug_x64.bat
@@ -1,4 +1,6 @@
 mkdir build
 cmake -S . -B build -A x64 -D CMAKE_BUILD_TYPE=Debug -D NO_AMPL_BINDINGS=1 ^
-    -D CMAKE_INSTALL_PREFIX:PATH="D:/Program Files/GSL"
+    -D CMAKE_INSTALL_PREFIX:PATH="%~dp0../../../3rd_party_libraries/GSL"
+cmake --build build --config Debug
+cmake --install build --config Debug
 pause
