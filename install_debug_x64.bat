938b654779934b4e0c777443f25eab64374eea8b
FakeMonkey <msolwooden@outlook.com>
2023-06-10 20:53:05 +0800

    将标准库的链接方式统一为动态发布版。构建时指明这一点。

--- install_debug_x64.bat
+++ install_debug_x64.bat
@@ -1,7 +1,7 @@
 mkdir build
 cmake -S . -B build -A x64 -D CMAKE_BUILD_TYPE=Debug -D NO_AMPL_BINDINGS=1 ^
     -D CMAKE_INSTALL_PREFIX:PATH="%~dp0../../../3rd_party_libraries/GSL" ^
-    -D GSL_DISABLE_TESTS:BOOL="1"
+    -D GSL_DISABLE_TESTS:BOOL="1" -D MSVC_RUNTIME_DYNAMIC:BOOL="ON"
 cmake --build build --config Debug
 cmake --install build --config Debug
 pause
